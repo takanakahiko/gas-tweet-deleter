234217b264d498b7f05ce4ff3afb3a7cc6d71460
takanakahiko <takanakahiko@gmail.com>
2019-03-08 01:37:13 +0900

    run lint --fix

--- src/twitter.ts
+++ src/twitter.ts
@@ -1,29 +1,34 @@
-export default class Twitter{
-  private service
-  constructor(consumerKey:string, consumerSecret:string, accessToken:string, accessTokenSecret:string){
+export default class Twitter {
+  private service;
+  constructor(
+    consumerKey: string,
+    consumerSecret: string,
+    accessToken: string,
+    accessTokenSecret: string
+  ) {
     // @ts-ignore
-    this.service =  OAuth1.createService('twitter')
+    this.service = OAuth1.createService('twitter')
       .setAccessTokenUrl('https://api.twitter.com/oauth/access_token')
       .setRequestTokenUrl('https://api.twitter.com/oauth/request_token')
       .setAuthorizationUrl('https://api.twitter.com/oauth/authorize')
       .setConsumerKey(consumerKey)
       .setConsumerSecret(consumerSecret)
-      .setAccessToken(accessToken, accessTokenSecret)
+      .setAccessToken(accessToken, accessTokenSecret);
   }
 
-  post(endPoint:string, payload:Object){
-    const response = this.service.fetch(`https://api.twitter.com/1.1/${endPoint}.json`,{
+  post(endPoint: string, payload: Object) {
+    const response = this.service.fetch(`https://api.twitter.com/1.1/${endPoint}.json`, {
       method: 'POST',
       contentType: 'application/json',
-      payload : JSON.stringify(payload)
+      payload: JSON.stringify(payload)
     });
     return JSON.parse(response.getContentText());
   }
 
-  get(endPoint:string, payload:Map<string,string>){
-    const response = this.service.fetch(`https://api.twitter.com/1.1/${endPoint}.json`,{
+  get(endPoint: string, payload: Map<string, string>) {
+    const response = this.service.fetch(`https://api.twitter.com/1.1/${endPoint}.json`, {
       method: 'GET',
-      payload : payload
+      payload: payload
     });
     return JSON.parse(response.getContentText());
   }
--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,6 @@
-export function getDayFormat(date = new Date()):string{
+export function getDayFormat(date = new Date()): string {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
   return `${year}-${month}-${day}`;
-};
+}
